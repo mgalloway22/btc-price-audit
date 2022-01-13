e93c7b82afa6ec97f71ee269a108bdd19c342fcd
Matt Galloway <mattgalloway22@me.com>
2022-01-12 20:00:48 -0600

    Update script to gather historical data between range

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Overview
-Simple tool to audit price history of `BTC-USD` and write to CSV
+Hisotrical price auditor for BTC
 
 ## Set up
 
 ```bash
-$ pip3 install requirements.txt
+$ pip3 install -r requirements.txt
 ```
 
 No user or authentication is required. All quote information is obtained via the free publicly exposed API.
@@ -17,18 +17,16 @@ To run the application simpley run the following command:
 $ python3 btc-price-audit.py
 ```
 
-This script will automatically create a csv file (if not exists) named `price-history.csv`. The script will then append a new line to the file approximately every 60 minutes with quote information for `BTC-USD`.
+This script will automatically create a csv file (and replace if exists) named `price-history.csv`. The script will accept a start and end date, and generate an hourly price history for `BTC-USD` between start and end date. 
+
+## Important Caveats
+
+* This report will include all 24 hours for both the start and end date (if applicable - obviously can't tell the future)
+* coinbase API has a limit to 200 bucket aggregations. This means that the max number of days this report can generate is 8 
+* this script assumes US/Central as the timezone
 
 ## Data Gathered
 
 This script will gather the following data every 60 minutes for `BTC-USD` organized into CSV columns:
 * `time`
 * `price`
-* `size`
-* `bid`
-* `ask`
-* `volume`
-
-## Failure States
-
-If the call to get the price quote fails for any reason (sometimes the connection can be closed unexpectedly), the script will print the error and try again in 5 seconds.
--- btc-price-auditor.py
+++ btc-price-auditor.py
@@ -1,48 +1,64 @@
 import cbpro
 import csv
 import os.path
-import time
+import datetime
+import pytz
 
 
 # constants
 FILE_NAME = 'price-history.csv'
 BTC_TICKER = 'BTC-USD'
-COLUMNS = ['time','price','size','bid','ask','volume']
-
-# grab price snapshot and write all the columns from the snapshot
-def writeRow(public_client: cbpro.PublicClient):
-    with open(FILE_NAME, 'a+') as file:
-        writer = csv.writer(file)
-        price_snapshot = public_client.get_product_ticker(BTC_TICKER)
-        row = []
-        for col in COLUMNS:
-            row.append(price_snapshot[col])
-        writer.writerow(row)
-
-# wrapper for writing rows to catch any possible exceptions
-def writeRowCatchingExceptions(public_client: cbpro.PublicClient):
-    try:
-        writeRow(public_client)
-    except BaseException as error:
-        print('Failed to add audit row to file, going to attempt to retry in 5 seconds: {}'.format(error))
-        # wait 5 seconds then retry
-        time.sleep(5)
-        writeRowCatchingExceptions(public_client)
+COLUMNS = ['time','price']
 
 def main():
     public_client = cbpro.PublicClient()
 
-    # check if file exists, if not create and add headers
-    if not os.path.exists(FILE_NAME):
-        with open(FILE_NAME, 'a+') as file:
-            writer = csv.writer(file)
-            writer.writerow(COLUMNS)
+    print("Please enter the start date and end date to generate the report. The max range is 8 days (limitation in coinbase's API).")
+    print("NOTE: this report will include all 24 hours (if applicable - obviously can't tell the future) for both the start and end date")
+    print("NOTE: this report uses US/Central timezone")
 
-    # begin looping writing rows to csv
-    while 1:
-        writeRowCatchingExceptions(public_client)
-        # wait 60 minutes
-        time.sleep(3600)
+    # gather input from user
+    while True:
+        try:
+            start_date_input = input('Enter the start date in YYYY-MM-DD format: ')
+            start_year, start_month, start_day = map(int, start_date_input.split('-'))
+            start_date = datetime.datetime(start_year, start_month, start_day,tzinfo=pytz.timezone('US/Central'))
+            
+            end_date_input = input('Enter the end date in YYYY-MM-DD format: ')
+            end_year, end_month, end_day = map(int, end_date_input.split('-'))
+            # add one to the end date to be able to capture that whole day
+            end_date = datetime.datetime(end_year, end_month,end_day + 1,tzinfo=pytz.timezone('US/Central'))
+        except Exception:
+            print("Error parsing date, please use the following format: YYYY-MM-DD")
+            continue
+        if start_date>=end_date:
+            print("Invalid range: start date is not before end date")
+        elif (end_date - start_date).days > 8:
+            # max api aggregation is 200 buckets, meaning max amount of days would be 8 days)
+            print("Invalid range: start date and end date are more thant 8 days apart (limitation of coinbase API)")
+        else:
+            break
+
+    # gater aggregation buckets
+    buckets = public_client.get_product_historic_rates(BTC_TICKER,start=start_date.isoformat(),end=end_date.isoformat(),granularity=3600)
+
+    # format into rows
+    rows = []
+    for bucket in buckets:
+        date_time = datetime.datetime.fromtimestamp(bucket[0]).isoformat()
+        price = bucket[3]
+        rows.append([date_time, price])
+
+    # reverse rows so newest rows are at the end
+    rows.reverse()
+
+    print("Generating file - will overwrite any existing file")
+    with open(FILE_NAME, 'w') as file:
+        # write headers
+        writer = csv.writer(file)
+        writer.writerow(COLUMNS)
+        writer.writerows(rows)
+    print("Report price-history.csv have been generated")
 
 if __name__ == '__main__':
     main()
